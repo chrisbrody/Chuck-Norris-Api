721b448548b524a6f78eebb946e1b048d86648b3
Chris Brody <chrisbrody@codifyacademy.com>
2017-05-16 21:51:17 -0700

    added css for background

--- js/main.js
+++ js/main.js
@@ -27,6 +27,7 @@ console.log(jokeArray);
 
 // function to display all jokes
 function viewalljokes() {
+  changeColors()
   // loop through jokeArray
   for (var i = 0; i < jokeArray.length; i++) {
     // create elements to add text too
@@ -56,6 +57,7 @@ document.getElementById("hidealljokes").addEventListener("click", hidealljokes, 
 
 // function for random jokes
 function randomjoke(){
+  changeColors()
   // get random number based on length of joke array
   var random = Math.floor(Math.random() * jokeArray.length);
 
@@ -81,3 +83,74 @@ document.addEventListener("keypress", function() {
   // get a random joke when any key is pressed
   randomjoke();
 });
+
+
+var nextHue = Math.floor(Math.random() * 360);
+changeColors()
+function changeColors() {
+  /*var hash = Math.abs(joke.split("").reduce(function(a, b) {
+    a = ((a << 5) - a) + b.charCodeAt(0);
+    return a & a;
+  }, 0));*/
+  var hue = nextHue,
+      sat = 65,
+      loval = 60,
+      hival = 75,
+      dark = hsl2rgb(hue, sat, loval),
+      light = hsl2rgb(hue, sat, hival);
+
+  $('body').css('background-color', 'rgb(' + light.r +','+ light.g +','+ light.b + ')');
+  $('div#content').css('background-color', 'rgb(' + dark.r +','+ dark.g +','+ dark.b + ')');
+  nextHue = (nextHue + 10) % 360;
+}
+
+function hsl2rgb(h, s, l) {
+
+    var r, g, b, m, c, x
+
+    if (!isFinite(h)) h = 0
+    if (!isFinite(s)) s = 0
+    if (!isFinite(l)) l = 0
+
+    h /= 60
+    if (h < 0) h = 6 - (-h % 6)
+    h %= 6
+
+    s = Math.max(0, Math.min(1, s / 100))
+    l = Math.max(0, Math.min(1, l / 100))
+
+    c = (1 - Math.abs((2 * l) - 1)) * s
+    x = c * (1 - Math.abs((h % 2) - 1))
+
+    if (h < 1) {
+        r = c
+        g = x
+        b = 0
+    } else if (h < 2) {
+        r = x
+        g = c
+        b = 0
+    } else if (h < 3) {
+        r = 0
+        g = c
+        b = x
+    } else if (h < 4) {
+        r = 0
+        g = x
+        b = c
+    } else if (h < 5) {
+        r = x
+        g = 0
+        b = c
+    } else {
+        r = c
+        g = 0
+        b = x
+    }
+
+    m = l - c / 2
+    r = Math.round((r + m) * 255)
+    g = Math.round((g + m) * 255)
+    b = Math.round((b + m) * 255)
+    return { r: r, g: g, b: b };
+}
